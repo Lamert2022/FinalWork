83cf4c2cfe798fa7e906b4d6beb65e48963ba8e1
Dmitriy <118912417+Lamert2022@users.noreply.github.com>
2023-01-26 18:29:11 +0300

    code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string[] originArray = {
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight"
+};
+
+int finalArraySize = 0;
+int finalArrayIndx = 0;
+
+for (int i = 0; i < originArray.Length; i++)
+    if (originArray[i].Length <= 3) finalArraySize++;
+
+string[] finalArray = new string[finalArraySize];
+
+for (int i = 0; i < originArray.Length; i++)
+    if (originArray[i].Length <= 3)
+        finalArray[finalArrayIndx++] = originArray[i];
+
+Console.Write("Исходный массив: ");
+for (int i = 0; i < originArray.Length; i++)
+    Console.Write(originArray[i] + ((i != originArray.Length-1) ? ", " : ""));
+Console.WriteLine();
+
+Console.Write("Результирующий массив: ");
+for (int i = 0; i < finalArray.Length; i++)
+    Console.Write(finalArray[i] + ((i != finalArray.Length-1) ? ", " : ""));
